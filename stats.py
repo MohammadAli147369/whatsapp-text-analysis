ca351aff4ee00175d5b1f327cb725224677264ca
MohammadAli147369 <102914446+MohammadAli147369@users.noreply.github.com>
2024-05-23 21:39:24 +0530

    Add files via upload

---
+++ stats.py
@@ -0,0 +1,139 @@
+from urlextract import URLExtract
+import pandas as pd
+import re
+
+
+extract = URLExtract()
+
+
+def fetchstats(selected_user, df):
+
+    # if the selected user is a specific user,then make changes in the dataframe,else do not make any changes
+
+    if selected_user != 'Overall':
+        df = df[df['User'] == selected_user]
+
+    num_messages = df.shape[0]
+    words = []
+    for message in df['Message']:
+        words.extend(message.split())
+
+    # counting the number of media files shared
+
+    mediaommitted = df[df['Message'] == '<Media omitted>']
+
+    # number of links shared
+
+    links = []
+    for message in df['Message']:
+        links.extend(extract.find_urls(message))
+
+    return num_messages, len(words), mediaommitted.shape[0], len(links)
+
+
+
+
+
+# most busy users {group level}
+
+def fetchbusyuser(df):
+
+    df = df[df['User'] != 'Group Notification']
+    count = df['User'].value_counts().head()
+
+    newdf = pd.DataFrame((df['User'].value_counts()/df.shape[0])*100)
+    return count, newdf
+
+
+from collections import Counter
+
+
+
+
+
+
+
+# get most common words,this will return a dataframe of
+# most common words
+
+def getcommonwords(selecteduser, df):
+
+    # getting the stopwords
+
+    file = open('stop_hinglish.txt', 'r')
+    stopwords = file.read()
+    stopwords = stopwords.split('\n')
+
+    if selecteduser != 'Overall':
+        df = df[df['User'] == selecteduser]
+
+    temp = df[(df['User'] != 'Group Notification') |
+              (df['User'] != '<Media omitted>')]
+
+    words = []
+
+    for message in temp['Message']:
+        for word in message.lower().split():
+            if word not in stopwords:
+                words.append(word)
+
+    mostcommon = pd.DataFrame(Counter(words).most_common(20))
+    return mostcommon
+
+
+# Word cloud
+
+
+def getemojistats(selecteduser, df):
+    if selecteduser != 'Overall':
+        df = df[df['User'] == selecteduser]
+
+    emojis = []
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               "]+", flags=re.UNICODE)
+
+    for message in df['Message']:
+        emojis.extend(emoji_pattern.findall(message))
+
+    emojidf = pd.DataFrame(Counter(emojis).most_common(len(Counter(emojis))), columns=['Emoji', 'Count'])
+    return emojidf
+
+
+def monthtimeline(selecteduser, df):
+
+    if selecteduser != 'Overall':
+        df = df[df['User'] == selecteduser]
+
+    temp = df.groupby(['Year', 'Month_num', 'Month']).count()[
+        'Message'].reset_index()
+
+    time = []
+    for i in range(temp.shape[0]):
+        time.append(temp['Month'][i]+"-"+str(temp['Year'][i]))
+
+    temp['Time'] = time
+
+    return temp
+
+
+def monthactivitymap(selecteduser, df):
+
+    if selecteduser != 'Overall':
+        df = df[df['User'] == selecteduser]
+
+    return df['Month'].value_counts()
+
+
+def weekactivitymap(selecteduser, df):
+
+    if selecteduser != 'Overall':
+        df = df[df['User'] == selecteduser]
+
+    return df['Day_name'].value_counts()
+
